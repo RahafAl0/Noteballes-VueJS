1b398aeddea7c2f8730bdc026779364f1e6b1330
RahafAl0 <ra.al9760@gmail.com>
2024-04-17 17:43:35 +0300

    getters & useRouter

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,54 +1,49 @@
 <template>
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
-      <div class="field">
-        <div class="control">
-          <textarea
-            v-model="newNote"
-            class="textarea"
-            placeholder="Add a new note"
-            ref="newNoteRef"
-          />
-        </div>
-      </div>
 
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            @click="addNote"
-            :disabled="!newNote"
-            class="button is-link has-background-success"
-          >
-            Add New Note
-          </button>
-        </div>
-      </div>
-    </div>
+    <AddEditNote
+      v-model="newNote"
+      placeholder="Add a new note"
+      ref="addEditNoteRef"
+    >
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!newNote"
+          class="button is-link has-background-success"
+        >
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
 
-    <Note 
-      v-for="note in storeNotes.notes" 
-      :key="note.id" 
-      :note="note" 
+    <Note
+      v-for="note in storeNotes.notes"
+      :key="note.id"
+      :note="note"
     />
+
   </div>
 </template>
 
 <script setup>
-  import { ref } from "vue";
-  import Note from "@/components/Notes/Note.vue"
-  import { useStoreNotes } from "@/stores/storeNotes.ts"
+  import { ref } from 'vue'
+  import Note from '@/components/Notes/Note.vue'
+  import AddEditNote from '@/components/Notes/AddEditNote.vue'
+  import { useStoreNotes } from '@/stores/storeNotes'
 
-  const storeNotes = useStoreNotes() 
 
-  const newNote = ref("");
-  const newNoteRef = ref(null);
 
+  const storeNotes = useStoreNotes()
+
+
+  const newNote = ref('')
+  const addEditNoteRef = ref(null)
 
   const addNote = () => {
     storeNotes.addNote(newNote.value)
-    newNote.value = ""
-    newNoteRef.value.focus();
-  };
-
+    newNote.value = ''
+    addEditNoteRef.value.focusTextarea()
+  }
 
 </script>
