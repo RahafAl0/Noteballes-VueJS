36f52eed482c10812e7424a5a0ebe96cdf8cfe10
RahafAl0 <ra.al9760@gmail.com>
2024-04-17 15:33:53 +0300

    use store

--- src/stores/storeNotes.ts
+++ src/stores/storeNotes.ts
@@ -8,7 +8,7 @@ export const useStoreNotes = defineStore('storeNotes', {
         {
           id: "id1",
           content:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime.",
         },
         {
           id: "id2",
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -26,7 +26,7 @@
     </div>
 
     <Note 
-      v-for="note in notes" 
+      v-for="note in storeNotes.notes" 
       :key="note.id" 
       :note="note" 
       @delete-note="deleteNote"
@@ -35,23 +35,15 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import Note from "@/components/Notes/Note.vue";
+  import { ref } from "vue";
+  import Note from "@/components/Notes/Note.vue"
+  import { useStoreNotes } from "@/stores/storeNotes.ts"
+
+  const storeNotes = useStoreNotes() 
 
   const newNote = ref("");
   const newNoteRef = ref(null);
 
-  const notes = ref([
-    {
-      id: "id1",
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.",
-    },
-    {
-      id: "id2",
-      content: "short notes",
-    },
-  ]);
 
   const addNote = () => {
     let currentDate = new Date().getTime(),
