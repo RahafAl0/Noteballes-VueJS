424848046a9ed11241fe21cd0e39369661e27480
RahafAl0 <ra.al9760@gmail.com>
2024-04-18 19:29:58 +0300

    install and setup firebase

---
+++ src/ts/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBK2laqQm5IqZYTITypvtir7VY1dz74Zys",
+  authDomain: "noteballs-b874b.firebaseapp.com",
+  projectId: "noteballs-b874b",
+  storageBucket: "noteballs-b874b.appspot.com",
+  messagingSenderId: "692968752097",
+  appId: "1:692968752097:web:4ff40e70021d7d363f88a0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
