54298a10d7d2c20d94db96dda30a05a3581cd227
RahafAl0 <ra.al9760@gmail.com>
2024-04-19 18:56:43 +0300

    auth form

--- src/views/ViewAuth.vue
+++ src/views/ViewAuth.vue
@@ -1,40 +1,79 @@
 <template>
   <div class="tabs is-centered">
-    
     <ul>
-      <li :class="{ 'is-active' : !register }">
+      <li :class="{ 'is-active': !register }">
         <a @click.prevent="register = false">Login</a>
       </li>
-      <li :class="{ 'is-active' : register }">
+      <li :class="{ 'is-active': register }">
         <a @click.prevent="register = true">Register</a>
       </li>
     </ul>
   </div>
 
   <div class="card auth-form">
-  <div class="card-content">
-    <div class="title has-text-centered">
-      {{ formTitle }}
-    </div>
-    <div class="content">
-      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec
-      id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus
-      et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis
-      consectetur purus sit amet fermentum.
+    <div class="card-content">
+      <div class="title has-text-centered">
+        {{ formTitle }}
+      </div>
+      <form @submit.prevent="onSubmot">
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input
+              v-model="credential.email"
+              class="input"
+              type="email"
+              placeholder="e.g. alexsmith@gmail.com"
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input
+              v-model="credential.password"
+              class="input"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+        </div>
+        <div class="field is-grouped is-grouped-right">
+          <p class="control">
+            <button class="button is-primary">
+              {{ formTitle }}
+            </button>
+          </p>
+        </div>
+      </form>
     </div>
   </div>
-</div>
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
+import { computed, ref, reactive } from "vue";
 
 const register = ref(false);
 
+const formTitle = computed(() => (register.value ? "Register" : "Login"));
 
-  const formTitle = computed(() => (register.value ? "Register" : "Login"));
+const credential = reactive({
+  email: "",
+  password: "",
+});
+
+const onSubmot = () => {
+  if (!credential.email || !credential.password) {
+    alert("Please fill in all fields");
+  } else {
+    if (register.value) {
+      console.log("Register", credential);
+    } else {
+      console.log("Login", credential);
+    }
+  }
+};
 </script>
-
 
 <style>
 .auth-form {
